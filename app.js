ab97f94decb37c5ff89fe7b58db3e1fa5d765d73
ratmadja <ratmadja@gmail.com>
2018-06-07 18:57:17 -0700

    simpleToDo-part2: Completed

--- app.js
+++ app.js
@@ -1,48 +1,65 @@
-
-
 function onReady() {
 
+  //1 Assign form to JS variable, declare toDos array
   const ADD_TODO_FORM = document.getElementById('addToDoForm');
-  const NEW_TODO_TEXT = document.getElementById('newToDoText');
-  const TODO_LIST = document.getElementById('toDoList');
+  let toDos = [];
+  let id = 0;
 
-  //Add an event listener
-  //37:14 When using (event) => shorthand as your event handler, it references the entire DOM window
-  //37:14 When using function(event) as your event handler, it references the item the event happened to
+  //4 Define renderTheUI - renders <ul>, but does not actually changes the array
+  function renderTheUI() {
+    const TODO_LIST = document.getElementById('toDoList');
+    TODO_LIST.textContent = ""; //Tells renderTheUI to clear the array content
 
-  ADD_TODO_FORM.addEventListener('submit', (event) => {
+    toDos.forEach(function(toDo){  //Where toDo argument first appears
+      const NEW_LI = document.createElement('li'); //creates empty <li>
+      const CHECKBOX = document.createElement('input'); //create empty input box
+      CHECKBOX.type = "checkbox"; //set input type to checkbox
+
+      const DELETE_BTN = document.createElement('button');
+      DELETE_BTN.textContent = "Delete!";
+
+      //5 Create DELETE_BTN. Remember the button is inside a forEach, so it's assigned to a specific toDo
+      DELETE_BTN.addEventListener('click', function(event) {
+        toDos = toDos.filter(function(item) {
+          //Everything that is not the delete button I just clicked on
+          //Gets passed to a new array
+          return item.id !== toDo.id;
+        });
+        renderTheUI();
+      });
+
+      NEW_LI.textContent = toDo.title; //JS knows toDo is an array object because forEach is applied to toDos array
+
+      TODO_LIST.appendChild(NEW_LI);
+      NEW_LI.appendChild(CHECKBOX);
+      NEW_LI.appendChild(DELETE_BTN);
+      })
+    }
+
+  //2 Push NEW_TODO_TEXT.value to the array
+  function createNewToDo() {
+      const NEW_TODO_TEXT = document.getElementById('newToDoText');
+      if (!NEW_TODO_TEXT.value){return; } //prevent empty values from being passed to the array
+      toDos.push({
+        title: NEW_TODO_TEXT.value,
+        complete: false,
+        id: id
+      });
+      id++;
+
+      NEW_TODO_TEXT.value = "";
+      renderTheUI();   //Be sure to call renderTheUI here
+    }
+
+  //3 On submit, push to array
+  ADD_TODO_FORM.addEventListener('submit', function(event) {
     event.preventDefault();
-
-
-    //get the text
-    let title = NEW_TODO_TEXT.value;
-    //create a new li in javascript
-    let newLi = document.createElement('li'); //creates <li></li>
-
-    //create a new input box in javascript
-    let checkbox = document.createElement('input');
-    //set the javascript input's type to checkbox
-    checkbox.type = "checkbox";
-    //add delete button
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
-
-    newLi.textContent = title;
-    newLi.appendChild(checkbox);
-    newLi.appendChild(deleteBtn);
-    TODO_LIST.appendChild(newLi);
-
-    deleteBtn.addEventListener('click', function(event) {
-      // console.log(event);
-      //the parent of the delete button is newLi
-      TODO_LIST.removeChild(this.parentElement);
-    });
-
-    //empty the input
-    NEW_TODO_TEXT.value="";
-
+    createNewToDo();
   });
-};
+
+  renderTheUI();
+
+}
 
 window.onload = function() {
   onReady();
